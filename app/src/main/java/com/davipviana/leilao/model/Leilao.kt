110721884c079fb055ea6d46a6f804ba940212a3
davi.viana <davi.viana@dtidigital.com.br>
2019-02-03 16:30:57 -0200

    Refatoração metodo proporLance

--- app/src/main/java/com/davipviana/leilao/model/Leilao.kt
+++ app/src/main/java/com/davipviana/leilao/model/Leilao.kt
@@ -17,13 +17,20 @@ class Leilao(val descricao: String) : Serializable {
     }
 
     fun proporLance(lance: Lance) {
-        val maiorLance = this.maiorLance;
-        val menorLance = this.menorLance;
+        calcularMaiorLance(lance)
+        calcularMenorLance(lance)
+    }
 
-        if(maiorLance == null || lance.valor > maiorLance)
-            this.maiorLance = lance.valor
-        if(menorLance == null || lance.valor < menorLance)
+    private fun calcularMenorLance(lance: Lance) {
+        val menorLance = this.menorLance;
+        if (menorLance == null || lance.valor < menorLance)
             this.menorLance = lance.valor
+    }
+
+    private fun calcularMaiorLance(lance: Lance) {
+        val maiorLance = this.maiorLance;
+        if (maiorLance == null || lance.valor > maiorLance)
+            this.maiorLance = lance.valor
     }
 
 }
